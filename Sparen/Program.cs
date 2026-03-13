6a79b57212de7dbf223c141bb9d1da728c4cb397
Marlies Dens <marlies.dens@pxl.be>
2026-03-13 14:10:10 +0100

    Add project files.

---
+++ Sparen/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Sparen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Sparen");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Write("Zakgeld (per week): ");
+            decimal.TryParse(Console.ReadLine(), out decimal pocketMoney);
+
+            Console.Write("Wekelijkse verhoging: ");
+            decimal.TryParse(Console.ReadLine(), out decimal increase);
+
+            Console.Write("Gewenst bedrag (doel): ");
+            decimal.TryParse(Console.ReadLine(), out decimal purpose);
+
+            Console.WriteLine();
+
+            int week = 1;
+            decimal newIncrease = increase;
+            
+                       
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write($"Week {week}: {pocketMoney*week} + {newIncrease} = {pocketMoney*week + newIncrease}");
+                Console.WriteLine();
+
+                
+                week += 1;
+                newIncrease += increase;
+                
+
+            } while ((pocketMoney*week + newIncrease) <= purpose);
+
+            Console.Write($"Week {week}: {pocketMoney * week} + {newIncrease} = {pocketMoney * week + newIncrease}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Je hebt {week} weken nodig om je doel van {purpose} euro bij elkaar te sparen.");
+            Console.ResetColor();
+
+
+        }
+    }
+}
