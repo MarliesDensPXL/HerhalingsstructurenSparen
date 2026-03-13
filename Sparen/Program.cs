184dc53f227dfbd2bfedcfa224601bfb967882ef
Marlies Dens <marlies.dens@pxl.be>
2026-03-13 14:48:23 +0100

    oplossing volgens 'voorbeeld' ipv volgens gif

--- Sparen/Program.cs
+++ Sparen/Program.cs
@@ -20,30 +20,36 @@
 
             Console.WriteLine();
 
-            int week = 1;
-            decimal newIncrease = increase;
-            
-                       
+
+            int week = 0;
+            decimal total = pocketMoney;
 
             do
             {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.Write($"Week {week}: {pocketMoney*week} + {newIncrease} = {pocketMoney*week + newIncrease}");
+                Console.Write($"Week {week}\t");
+                Console.Write($"Zakgeld {pocketMoney}\t");
+
+                if (!(week == 0))
+                {
+                    total += pocketMoney;
+                }
+                               
+
+
+                Console.Write($"Totaal: {total}");
                 Console.WriteLine();
 
-                
                 week += 1;
-                newIncrease += increase;
-                
+                pocketMoney += increase;
 
-            } while ((pocketMoney*week + newIncrease) <= purpose);
+            } while (total < purpose);
 
-            Console.Write($"Week {week}: {pocketMoney * week} + {newIncrease} = {pocketMoney * week + newIncrease}");
+
 
             Console.WriteLine();
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Je hebt {week} weken nodig om je doel van {purpose} euro bij elkaar te sparen.");
+            Console.WriteLine($"Je hebt {week - 1} weken nodig om je doel van {purpose} euro bij elkaar te sparen.");
             Console.ResetColor();
 
 
